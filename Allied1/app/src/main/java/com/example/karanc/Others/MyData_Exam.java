1cdd04b6aa7b40e5d0e11e9434a16b9093f2c2ac
kavanhajare <32415553+kavanhajare@users.noreply.github.com>
2022-05-21 18:09:23 +0530

    Delete Allied1 directory

--- Allied1/app/src/main/java/com/example/karanc/Others/MyData_Exam.java
+++
@@ -1,8 +0,0 @@
-package com.example.karanc.Others;
-
-/**
- * Created by karanc on 14-06-2016.
- */
-public class MyData_Exam {
-    public static String[] examname={"GRE","GATE","CAT","TOEFL","IELTS","UPSC","PSU"};
-}
